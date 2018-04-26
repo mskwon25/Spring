920fe73c4ba8f9c01e1154ff9a363004e109dcac
mskwon25 <canopus.kwon@gmail.com>
2018-04-26 21:04:18 +0900

    make UserDao.java file

---
+++ src/main/java/com/spring/bootcamp/dao/UserDao.java
@@ -0,0 +1,48 @@
+package com.spring.bootcamp.dao;
+
+import com.spring.bootcamp.domain.User;
+
+import java.sql.*;
+
+/**
+ * @author minseok.kwon@nhnent.com
+ * @since 2018-04-26
+ */
+public class UserDao {
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/book", "root", "eodeoddl7@");
+
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/book", "root", "eodeoddl7@");
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
