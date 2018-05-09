537d798148972d93ae5e07d5aed02000c1f27b3e
mskwon25 <canopus.kwon@gmail.com>
2018-05-09 22:26:06 +0900

    update datasource

--- src/main/java/com/spring/bootcamp/dao/DaoFactory.java
+++ src/main/java/com/spring/bootcamp/dao/DaoFactory.java
@@ -1,8 +1,10 @@
 package com.spring.bootcamp.dao;
 
-import com.spring.bootcamp.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * @author minseok.kwon@nhnent.com
@@ -14,8 +16,20 @@ public class DaoFactory {
     @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
+        userDao.setDataSource(dataSource());
         return userDao;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/book");
+        dataSource.setUsername("root");
+        dataSource.setPassword("eodeoddl7@");
+
+        return dataSource;
     }
 
     @Bean
